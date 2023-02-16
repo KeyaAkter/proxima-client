6a9ba7894fd5a562faeaadc4164b6e27668b4680
KeyaAkter <sanhatanvin12@gmail.com>
2023-02-16 21:47:56 +0600

    context api setup initialized

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { ProjectContextProvider } from "./context/ProjectContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ProjectContextProvider>
+        <App />
+      </ProjectContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
