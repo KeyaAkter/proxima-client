138a7e8e9977b91a09bca9661143aff62d8b191b
KeyaAkter <sanhatanvin12@gmail.com>
2023-02-26 23:16:06 +0600

    login and signup page added

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { ProjectContextProvider } from "./context/ProjectContext";
+import { AuthContextProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <ProjectContextProvider>
-        <App />
+        <AuthContextProvider>
+          <App />
+        </AuthContextProvider>
       </ProjectContextProvider>
     </BrowserRouter>
   </React.StrictMode>
