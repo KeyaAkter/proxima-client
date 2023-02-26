3dea46ea28d474107d82929ff7c442416732d1e8
KeyaAkter <sanhatanvin12@gmail.com>
2023-02-26 23:19:12 +0600

    navbar login and signup link added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,15 @@ const Navbar = () => {
       <Link to="/" className="logo text-2xl font-medium text-sky-400">
         Proxima
       </Link>
+
+      <nav className="flex gap-5">
+        <Link to="/login" className="hover:text-sky-400 duration-300">
+          Login
+        </Link>
+        <Link to="/signup" className="hover:text-sky-400 duration-300">
+          Signup
+        </Link>
+      </nav>
     </div>
   );
 };
